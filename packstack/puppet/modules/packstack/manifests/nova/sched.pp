30cae2a3a6c28842ceabd2a4ddde43254ae811c2
Javier Pena <jpena@redhat.com>
2016-06-10 15:29:50 +0200

    Fix Nova sched filter

--- packstack/puppet/modules/packstack/manifests/nova/sched.pp
+++ packstack/puppet/modules/packstack/manifests/nova/sched.pp
@@ -1,13 +1,14 @@
 class packstack::nova::sched () 
 {
-    nova_config{
+    class {'::nova::scheduler::filter':
       # OpenStack doesn't include the CoreFilter (= CPU Filter) by default
-      'DEFAULT/scheduler_default_filters':
-          value => 'RetryFilter,AvailabilityZoneFilter,RamFilter,ComputeFilter,ComputeCapabilitiesFilter,ImagePropertiesFilter,CoreFilter';
-      'DEFAULT/cpu_allocation_ratio':
-          value => hiera('CONFIG_NOVA_SCHED_CPU_ALLOC_RATIO');
-      'DEFAULT/ram_allocation_ratio':
-          value => hiera('CONFIG_NOVA_SCHED_RAM_ALLOC_RATIO');
+      scheduler_default_filters => ['RetryFilter', 'AvailabilityZoneFilter',
+                                    'RamFilter', 'DiskFilter' , 'ComputeFilter',
+                                    'ComputeCapabilitiesFilter', 'ImagePropertiesFilter',
+                                    'ServerGroupAntiAffinityFilter',
+                                    'ServerGroupAffinityFilter', 'CoreFilter'],
+      cpu_allocation_ratio      => hiera('CONFIG_NOVA_SCHED_CPU_ALLOC_RATIO'),
+      ram_allocation_ratio      => hiera('CONFIG_NOVA_SCHED_RAM_ALLOC_RATIO'),
     }
 
     class { '::nova::scheduler':
